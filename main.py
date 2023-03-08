70b4844eefdf326a808639b6f45ee84d87f509b8
ayusikayumna <ayusikayumna1308@gmail.com>
2023-03-08 08:41:26 +0700

    menambah main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+
+
+#print(a)
+
+#coba = 19.6
+#print(coba)
+
+#aja = int(coba)
+#print(aja)
+
+#nilai = input('Masukkan nilai:\t')
+
+#print(nilai)
+
+#nilai2 = int(input('Masukkan nilai integer:.'))
+#nilai3 = int(input('Masukkan nilai tiga'))
+
+#hasil = nilai2 % nilai3
+#print(hasil)
+
+#x = 3
+#y = 9
+
+
+#if y < 3 :
+#    print("Ini salah")
+#else:
+#    print("Ini benar")
+
+#nilai = int(input("Masukkan nilai angkat matkul anda:\t"))
+#if nilai >= 85:
+#    print("Matkul anda mendapatkan nilai A")
+#elif nilai >=80:
+#    print("Matkul anda mendapat nilai A-")
+#elif nilai >=75:
+#    print("Matkul anda mendapat nilai B+")
+#elif nilai >=70:
+#    print("Matkul anda mendapat nilai B")
+#elif nilai >=65:
+#   print("Matkul anda mendapat nilai C+")
+#elif nilai >=60:
+#    print("Matkul anda mendapat nilai C")
+#elif nilai >=55:
+#    print("Matkul anda mendapat nilai D")
+
+#angkatan = [2018, 2019, 2020, 2021, 2022]
+
+#for 1 in angkatan:
+#    print("Ini angkatan{}".format(i))
+
+#nilai = 3
+
+#while nilai < 10:
+#    print("nilai kurang")
+#    nilai = nilai + 5
